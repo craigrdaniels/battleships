f0e8f8db5965569f367ea460c16fc04b7ba8bd39
Craig Daniels <craigrdaniels@gmail.com>
2022-08-30 21:59:21 +1000

    check surrounding spaces when attacking randomly

--- src/factories/playerFactory.js
+++ src/factories/playerFactory.js
@@ -6,6 +6,10 @@ class Player {
   }
 
   isValidAttack(player, x, y) {     //eslint-disable-line
+    // make sure falls within bounds of gameboard
+    if (x < 0 || x >= player.gameboard.board[0].length) return false;
+    if (y < 0 || y >= player.gameboard.board.length) return false;
+
     if (player.gameboard.misses[y][x]) return false;
   
     if (player.gameboard.board[y][x]) {
@@ -22,9 +26,33 @@ class Player {
     }
   }
 
-  // For now just attack randomly
-  // ToDo: add intelligence to pick around a 'hit' for less turns
   attackRandom(player) {
+    // check for any hits without sunk ships
+    // check surrounding spaces, if not a valid move then choose the next 'hit'
+    for (let i = 0; i < player.gameboard.board[0].length; i += 1) {
+      for (let j = 0; j < player.gameboard.board.length; j += 1) {
+        if (player.gameboard.board[j][i]) {
+          if (player.gameboard.board[j][i].hits[player.gameboard.getShipIndex(i, j)] === 'hit' && !player.gameboard.board[j][i].isSunk()) {
+
+            const attackVectors = [
+              [-1, 0],
+              [0, 1],
+              [1, 0],
+              [0, -1]
+            ];
+
+            for (let k = 0; k < attackVectors.length; k += 1) {
+              if (this.isValidAttack(player, i + attackVectors[k][0], j + attackVectors[k][1])) {
+                this.attack(player, i + attackVectors[k][0], j + attackVectors[k][1]);
+                return;
+              }
+            }
+          }
+        }
+      }
+    }
+
+
     let success = false;
     while (success === false) {
       const x = Math.floor(Math.random() * player.gameboard.board[0].length);
@@ -33,6 +61,7 @@ class Player {
       if (this.isValidAttack(player, x, y)) {
         this.attack(player, x, y);
         success = true;
+        return;
       }
     }
   }
